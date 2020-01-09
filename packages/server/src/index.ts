fa81cebafd513c9238e936a435b2c7c3cb827063
ericzakariasson <eric.zakariasson@gmail.com>
2020-01-09 18:47:36 +0100

    Create typeorm connection

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,21 +1,29 @@
-import 'reflect-metadata';
-import * as dotenv from 'dotenv';
-import * as express from 'express';
-import * as cookieParser from 'cookie-parser';
-import { ApolloServer } from 'apollo-server-express';
-import { generateSchema } from './schema';
+import "reflect-metadata";
+import * as dotenv from "dotenv";
+import * as express from "express";
+import * as cookieParser from "cookie-parser";
+import { ApolloServer } from "apollo-server-express";
+import { generateSchema } from "./schema";
+import { createConnection } from "typeorm";
+import { connectionOptions } from "./ormconfig";
 
-import * as user from './controllers/user';
+import * as user from "./controllers/user";
 
 dotenv.config();
 
 const startServer = async () => {
+  const connection = await createConnection(connectionOptions);
+
+  if (!connection) {
+    throw new Error("Connection to database could not be established");
+  }
+
   const app = express();
 
   app.use(cookieParser());
 
-  app.use('/auth/login', user.login);
-  app.use('/auth/callback', user.loginCallback);
+  app.use("/auth/login", user.login);
+  app.use("/auth/callback", user.loginCallback);
 
   const schema = await generateSchema();
   const server = new ApolloServer({
